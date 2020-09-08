f3ab0b62d4ca9ca35d3511cf1cfdc1f392aed619
Marcelo Di Liscia <diliscia@gmail.com>
2020-09-07 22:37:52 -0400

    Update Horse-Racing Duals.cpp

--- Horse-Racing Duals.cpp
+++ Horse-Racing Duals.cpp
@@ -12,6 +12,41 @@ using namespace std;
 int main()
 {
     vector<int> P;
+    int minDif = 10000000;
+    int N;
+    
+    cin >> N; cin.ignore();
+    for (int i = 0; i < N; i++) {
+        int Pi;
+        cin >> Pi; cin.ignore();
+        P.push_back(Pi);
+    }
+    sort(P.begin(), P.begin()+N);
+ 
+    for (int j = 0; j < N-1; ++j) {
+        if (P[j+1] - P[j] < minDif) {
+            minDif = P[j+1] - P[j];
+        }
+    }
+
+    cout << minDif << endl;
+}
+//-------------------------------------------
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    vector<int> P;
     int minDist = 10000000;
     int maxNr = 0;
     int minNr = 10000000;
