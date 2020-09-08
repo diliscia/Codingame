820b28b067fcd5e82ff1e8c2c14d47f35cd4528e
Marcelo Di Liscia <diliscia@gmail.com>
2020-09-07 22:25:43 -0400

    Update Horse-Racing Duals.py

--- Horse-Racing Duals.py
+++ Horse-Racing Duals.py
@@ -4,6 +4,31 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+minDif = 10000000  # Define the minimum difference
+P = []
+n = int(input())
+for i in range(n):
+    pi = int(input())
+    P.append(pi)
+P = sorted(P)
+
+for i in range(n-1):
+    if (P[i+1] - P[i] < minDif): #  
+        minDif = P[i+1] - P[i]
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(minDif)
+
+# --------------------------------------------
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
 P = []
 minDist = 10000000  # Define the minimum difference
 maxNr = 0   # Define the maximum Number of horse
