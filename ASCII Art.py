bfcb2add0d19fde00cb7a6ba625ae5a8c88ed7a8
Marcelo Di Liscia <diliscia@gmail.com>
2020-09-11 15:53:14 -0400

    Create ASCII Art.py

---
+++ ASCII Art.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l = int(input())    # length of each letter
+h = int(input())    # height of each letter
+t = input()         # word to be written in ASCII Art
+asciialfa = []      # list to register the ACII alphabet
+alfa = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ?'    # uppercase alphabet
+for height in range(h):
+    asciialfa.append(input())   # registering ASCII alphabet 
+for lines in range(l+1):
+    for letter in t:            # finding index of each letter in the word
+        idx = alfa.find(letter.capitalize())
+        if idx == -1:
+            idx = len(alfa)-1    
+        print(asciialfa[lines][idx*l:idx*l+l], end = '')
+    print()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
