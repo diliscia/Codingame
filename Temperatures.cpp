e40d50a88432936e5f3ee8283d191b012fed64e1
Marcelo Di Liscia <diliscia@gmail.com>
2021-03-09 17:34:13 -0500

    Update Temperatures.cpp

--- Temperatures.cpp
+++ Temperatures.cpp
@@ -21,7 +21,7 @@ int main()
         int t; // a temperature expressed as an integer ranging from -273 to 5526
         cin >> t; 
         cin.ignore();
-        dif2 = abs(t-0);    // calculate de difference
+        dif2 = abs(t-0);    // calculate absolute difference to reference
         if (dif2 < dif) {   // if is inferior 
             dif = dif2;     // keep this difference
             result = t;     // keep this temperatue
